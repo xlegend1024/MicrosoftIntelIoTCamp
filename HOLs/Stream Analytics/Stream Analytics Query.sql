d4528deaa9465d39b0387fda9f94d9ee92617e01
Hyun <hyunsuk.shin@live.com>
2017-09-12 12:45:47 -0700

    Update Stream Analytics Query.sql

--- HOLs/Stream Analytics/Stream Analytics Query.sql
+++ HOLs/Stream Analytics/Stream Analytics Query.sql
@@ -9,17 +9,3 @@ INTO
     sqldb
 FROM
     iothub
-
--- SELECT ONLY messages
--- WHERE the temperature is > 40
--- FROM the iot hub and put them 
--- INTO the alerts event hub
-SELECT
-    deviceID,
-    [timestamp] as [timestamp],
-    temperature
-INTO
-    alerts
-FROM
-    iothub
-WHERE temperature > 40  
